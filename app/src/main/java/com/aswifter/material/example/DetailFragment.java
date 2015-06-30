fd2a663165c848b3c3ab28669dd845c51777e88f
chenyc <chen.yangcun@xiaoyi.com>
2015-06-30 10:15:41 +0800

    提交DetailFragment

---
+++ app/src/main/java/com/aswifter/material/example/DetailFragment.java
@@ -0,0 +1,35 @@
+package com.aswifter.material.example;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.aswifter.material.R;
+
+/**
+ * Created by Chenyc on 2015/6/29.
+ */
+public class DetailFragment extends Fragment {
+
+    public static DetailFragment newInstance(String info) {
+        Bundle args = new Bundle();
+        DetailFragment fragment = new DetailFragment();
+        args.putString("info", info);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_detail, null);
+        TextView tvInfo = (TextView) view.findViewById(R.id.tvInfo);
+        tvInfo.setText(getArguments().getString("info"));
+        return view;
+    }
+}
