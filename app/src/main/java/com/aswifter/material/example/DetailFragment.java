fc775cff2f292b0d30a19efaa2c12eef76638abb
chenyc <chen.yangcun@xiaoyi.com>
2015-07-03 14:46:22 +0800

    add onClick on tvInfo

--- app/src/main/java/com/aswifter/material/example/DetailFragment.java
+++ app/src/main/java/com/aswifter/material/example/DetailFragment.java
@@ -2,6 +2,7 @@ package com.aswifter.material.example;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,6 +31,12 @@ public class DetailFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_detail, null);
         TextView tvInfo = (TextView) view.findViewById(R.id.tvInfo);
         tvInfo.setText(getArguments().getString("info"));
+        tvInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"hello",Snackbar.LENGTH_SHORT).show();
+            }
+        });
         return view;
     }
 }
